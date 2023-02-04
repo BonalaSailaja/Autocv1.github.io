a6e88ba86d51e8e208024b2a075a14cb8d7fb905
Sailaja <121034691+BonalaSailaja@users.noreply.github.com>
2023-02-04 14:48:27 +0530

    changes commited

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ form.addEventListener("submit", handleSubmit);
 
 // import all the fields from the form.
 // eg.
-// const name = document.getElementById("name");
+ const name = document.getElementById("name");
 
 function handleSubmit(e) {
   e.preventDefault();
   //   here get the values from the form and set it to the resume
   // eg.
-  //   const name_resume = document.getElementById("name_resume");
-  //   name_resume.innerHTML = name.value;
+    const name_resume = document.getElementById("name_resume");
+     name_resume.innerHTML = name.value;
   //   similarly for all the other fields
 }
 
